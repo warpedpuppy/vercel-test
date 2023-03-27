0f02c29193ac9577296dce4ebf0b9e39c8c8e252
ted <ted@warpedpuppy.com>
2023-03-27 12:34:34 -0400

    added cors

--- index.js
+++ index.js
@@ -2,6 +2,25 @@ const express = require('express');
 
 const app = express();
 
+const cors = require("cors");
+let allowedOrigins = [];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        let message =
+          "The CORS policy for this application does not allow access from origin " +
+          origin;
+        return callback(new Error(message), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
+
+
 app.get('/', (req, res) => {
 	res.send('success')
 })
