6ae201aa4a4e8d6701494c0f8a02443d24ab3319
ted <ted@warpedpuppy.com>
2023-03-27 12:39:18 -0400

    added 3000 to cors

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 const cors = require("cors");
-let allowedOrigins = [];
+let allowedOrigins = ['http://localhost:3000/'];
 
 app.use(
   cors({
